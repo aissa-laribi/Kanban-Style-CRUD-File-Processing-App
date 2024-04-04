754986225e3ee67e8f52bbed6ef798b06d2c53af
Edward <edtrevor500@gmail.com>
2024-04-04 14:52:14 +0100

    Adding the base main file along with dummy functions to fill in.

---
+++ Functions3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+void Display_Board(){
+	printf("Dummy function");
+	fflush(stdout);
+}
+
+void Load_File(){
+	printf("Dummy function");
+	fflush(stdout);
+}
+
+void Edit_List(){
+	printf("Dummy function");
+	fflush(stdout);
+}
+
+void Edit_Board(){
+	printf("Dummy function");
+	fflush(stdout);
+}
+
+void Save_Board(){
+	printf("Dummy function");
+	fflush(stdout);
+}
+
