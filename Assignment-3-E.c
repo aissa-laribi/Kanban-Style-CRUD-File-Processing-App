9614022053664cf0f3fafb776fe3835cf4e4de94
Aissa Laribi <aoifemullen@aoifes-air.home>
2024-04-05 01:51:27 +0100

    File once loaded then reusable, complete Display_Board function, Edit_list match user input and list in file

--- Assignment-3-E.c
+++ Assignment-3-E.c
@@ -5,28 +5,38 @@
 int main(void) {
 	int choice = 0;
 
-	printf("Menu:\n1. Display Board\n2. Load Board From File\n3. Edit List\n4. Edit Board\n5. Save Board To A File\n6. Quit\nEnter Your Choice (1-6)\n");
+
+	printf("Menu:\n1. Display Board\n2. "
+			"Load Board From File\n3. Edit List\n4. "
+			"Edit Board\n5. Save Board To A File\n6. "
+			"Quit\nEnter Your Choice (1-6)\n");
 	fflush(stdout);
 	scanf("%d", &choice);
 
 	switch(choice){
 	case 1:
 		Display_Board();
+		main();
 		break;
 	case 2:
 		Load_Board();
+		main();
 		break;
 	case 3:
 		Edit_List();
+		main();
 		break;
 	case 4:
 		Edit_Board();
+		main();
 		break;
 	case 5:
 		Save_Board();
+		main();
 		break;
 	case 6:
 		printf("Exiting Program.");
+		main();
 		fflush(stdout);
 		break;
 	default:
--- Functions3.c
+++ Functions3.c
@@ -3,65 +3,137 @@
 #include <string.h>
 #include <stdbool.h>
 
+static char file[80]; /*Static variable containing the name of file and reused anywhere in the program*/
+
+struct listnode{
+	char name[50];
+	struct listnode *next;
+};
+
+typedef struct listnode ListNode;
+typedef ListNode *ListNodePtr;
+
+
 void Display_Board(){
-	printf("Dummy function");
+	/*In the Detailled specification, it says if 1 (Display board) => print default.txt*/
+	char line[256];
+	FILE *fp;
+
+	if ((fp = fopen("default.txt", "r+"))== NULL) {
+		printf("File failed to be opened. Please enter the name again.\n");
+		fflush(stdout);
+	}
+	while(fgets(line, sizeof(line), fp)){
+			printf("%s", line);
+			fflush(stdout);
+		}
+	fclose(fp);
+
+	puts("");
 	fflush(stdout);
 }
 
 void Load_Board(){
-	char FILE_NAME[80];
 
 	printf("Please enter the name of the file you wish to enter. Enter 'default' for the default board.\n");
 	fflush(stdout);
-	scanf("%s", FILE_NAME);
+
+	scanf("%s",file);
+	printf("%s\n", file);
 
 
 	FILE *fp;
 
-	if ((fp = fopen(FILE_NAME, "r+"))== NULL) {
+	if ((fp = fopen(file, "r+"))== NULL) {
 		printf("File failed to be opened. Please enter the name again.\n");
 		fflush(stdout);
 	} else {
 		printf("File opened successfully.\n");
 		fflush(stdout);
 	}
+	fclose(fp);
+
 
 }
 
 void Edit_List(){
-	/*1. Edit an item
+	/*TODO:
+	 *1. Edit an item
 	  2. Add a new item
 	  3. Delete an item
 	  4. Return to main menu */
 
-	struct listnode{
-		char name[50];
-		struct listnode *next;
-	};
-
+	printf("Edit %s\n", file);
 	unsigned short int user_choice;
-	char first_name[50];
-	//char ch;
+	char input[50];
+	char line[256];
+	char ch;
+	FILE *fp;
 
-	typedef struct listnode ListNode;
-	typedef ListNode *ListNodePtr;
-
-	int getc(FILE *fp);
+	if((fp = fopen(file, "r+"))== NULL) {
+		printf("File failed to be opened. Please enter the name again.\n");
+		fflush(stdout);
+	}
 
 	printf("Enter the name of the list to edit:");
-	fgets(first_name, 50, stdin);
+	scanf("%s", input);
+	input[strlen(input)] = ':';
+	input[strlen(input)] = '\n';
+	printf("Looking for %s\n", input);
+	/*Testing printing input file*/
 
-	/*Null character*/
+	while(fgets(line, sizeof(line), fp)){
+		if(strcmp(input, line) == 0){
+			printf("found\n");
+			printf("Options:\n "
+					"1. Edit an item\n"
+					"2. Add a new item\n"
+					"3. Delete an item\n"
+					"4. Return to main menu\n"
+					"Enter your option: ");
+		scanf("%hd", &user_choice);
+		ch = getchar();
+		while(user_choice < 1 || user_choice > 4){
+			if(ch == '\n'){
+				puts("Invalid input");
+				printf("Options:\n"
+					   "1. Edit an item\n"
+					   "2. Add a new item\n"
+					   "3. Delete an item\n"
+					   "4. Return to main menu\n"
+					   "Enter your option: ");
+				scanf("%hd", &user_choice);
+						}
+			else{
+				/*Trap the unwanted characters here*/
+			}
+			ch = getchar();
+		}
 
-	/*Compare fname and
+		switch(user_choice){
+		case 1:
+			printf("Enter the name of the item to edit:\n");
+			fgets(input, 50, stdin);
+			input[strlen(input)] = '\n';
+			input[strlen(input)] = '\0';
+			printf("%s\n", input);
+			printf("%lu\n", strlen(input));
 
-	scanf("%hu", &user_choice);
-	ch = getchar();
-	while(user_choice < 1 || user_choice > 4){
-		print
+			/*Stuck here when comparing input let say Oculus Pro and cannot match it*/
+			while(fgets(line, sizeof(line), fp)){
+				printf("%lu", strlen(input));
+				if(strcmp(line, input) == 0){
+					puts("True");
+				}
+			}
+
+		}
+
+		}
 	}
-	fflush(stdout);
-	*/
+	fclose(fp);
+
+
 }
 
 void Edit_Board(){
@@ -73,4 +145,3 @@ void Save_Board(){
 	printf("Dummy function");
 	fflush(stdout);
 }
-
