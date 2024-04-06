4be2bcae8b6bd4c75add91b00e53af15cc7e7b09
Aissa Laribi <aoifemullen@aoifes-air.home>
2024-04-06 18:51:23 +0100

    Sorting merge without git

--- Assignment-3-E.c
+++ Assignment-3-E.c
@@ -2,26 +2,19 @@
 #include <stdlib.h>
 #include "Functions3.h"
 
-//test for head
 int main(void) {
 	int choice = 0;
 
-
-	printf("Menu:\n1. Display Board\n2. "
-			"Load Board From File\n3. Edit List\n4. "
-			"Edit Board\n5. Save Board To A File\n6. "
-			"Quit\nEnter Your Choice (1-6)\n");
+	printf("Menu:\n1. Display Board\n2. Load Board From File\n3. Edit List\n4. Edit Board\n5. Save Board To A File\n6. Quit\nEnter Your Choice (1-6)\n");
 	fflush(stdout);
 	scanf("%d", &choice);
 
 	switch(choice){
 	case 1:
 		Display_Board();
-		main();
 		break;
 	case 2:
 		Load_Board();
-		main();
 		break;
 	case 3:
 		Edit_List();
